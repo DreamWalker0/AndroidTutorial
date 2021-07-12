2b28d8534fb19f135944dabec395fcc7b54603b8
Jorge <conundrumparadox33@gmail.com>
2021-07-12 15:14:01 -0700

    2nd commit

--- app/src/main/java/com/example/layoutbaiscs2/MainActivity.kt
+++ app/src/main/java/com/example/layoutbaiscs2/MainActivity.kt
@@ -2,10 +2,8 @@ package com.example.layoutbaiscs2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.RadioButton
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.cyborg_signin.*
-import kotlinx.android.synthetic.main.txt_view_experiment.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +16,13 @@ class MainActivity : AppCompatActivity() {
          * This autogenerate id in the xml
          * so not need to use findBYId
          */
-        btnUplift.setOnClickListener {
-            val firstName = etFirstName.text.toString()
-            val lastName = etLastName.text.toString()
-            val bloodType = etBloodType.text.toString()
-            val cyborg = etCyborg.text.toString()
-            Log.d("MainActivity", "$firstName $lastName, blood type $bloodType, is a cyborg? $cyborg")
-        }
+//        btnUplift.setOnClickListener {
+//            val firstName = etFirstName.text.toString()
+//            val lastName = etLastName.text.toString()
+//            val bloodType = etBloodType.text.toString()
+//            val cyborg = etCyborg.text.toString()
+//            Log.d("MainActivity", "$firstName $lastName, blood type $bloodType, is a cyborg? $cyborg")
+//        }
 
         //This is the old way
         //val btnYes = findViewById<Button>(R.id.btnYes)
@@ -45,6 +43,32 @@ class MainActivity : AppCompatActivity() {
 //            val result = firstNumber + secondNumber
 //            tvResult.text = result.toString()
 //        }
+
+        //todo 2. IMAGEVIEW
+        //This adds an image of a fancy cat when a button is clicked
+//        btnAddImage.setOnClickListener {
+//            ivImageCat.setImageResource(R.drawable.fancy_cat)
+//        }
+
+        // todo 3. CheckBox, RadioButton
+        //When you click the button ingredients of burger get displayed
+//        btnOrder.setOnClickListener {
+//            val checkedMeatRadioButtonId = rgMeat.checkedRadioButtonId
+//            val meat = findViewById<RadioButton>(checkedMeatRadioButtonId)
+//            val cheese = cbCheese.isChecked
+//            val onions = cbOnions.isChecked
+//            val pepper = cbPepper.isChecked
+//            //This gets all the checked boxes
+//            val orderString = "You ordered a burger with:\n" +
+//                    "${meat.text}" +
+//                    (if(cheese)"\nCheese" else "") +
+//                    (if (onions)"\nOnions" else "") +
+//                    (if (pepper)"\nPeppers" else "")
+//            //The content of orderString is assigned to appear in the TextView tvOrder
+//            tvOrder.text = orderString
+//        }
+
+
 
 
     }
